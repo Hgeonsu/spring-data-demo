9c0e9e7ab4c1c7afb899b47c23a0f0fc29baf5bb
geonsu <ghdrjstn@gmail.com>
2021-06-10 14:31:27 +0900

    value 타입 맵핑

--- src/main/java/me/geonsu/springdatademo/Account.java
+++ src/main/java/me/geonsu/springdatademo/Account.java
@@ -22,6 +22,19 @@ public class Account {
     @Transient //객체에서는 사용하고, 컬럼에 맵핑은 하고 싶지 않을 때
     private String no;
 
+    //아래와 같이 그냥 쓰거나
+//    @Embedded
+//    private Address address;
+
+    //Address라는 valuetype의 street 필드를, home_street으로 바꿔 쓰고 싶을 때
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "home_street"))
+    })
+    private Address address;
+
+
+
     public Long getId() {
         return id;
     }
